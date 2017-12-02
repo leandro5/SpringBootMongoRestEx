6a6e0ccce4ccf6062eaaa568cfcf5606edc4fa92
Leandro Souza <leandroades@gmail.com>
2017-12-02 12:57:05 -0200

    Mais ajustes no projeto

--- SpringBootMongoRestEx/src/main/java/net/leandroabreu/springbootmongorestex/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package net.leandroabreu.springbootmongorestex;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ContatoRepository extends MongoRepository<Contato, String> {
-
-}
